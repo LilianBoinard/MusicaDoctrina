1be5050e73ce53f233da1cdb41aaea7ca3b95502
LilianBoinard <boinard.lilian@outlook.fr>
2025-03-24 20:10:04 +0100

    Homepage fix

--- src/Components/Template/Outlet/OutletContent.tsx
+++ src/Components/Template/Outlet/OutletContent.tsx
@@ -4,7 +4,7 @@ import {Outlet} from "react-router";
 export default function OutletContent(): ReactElement {
     return(
         <>
-            <div className="p-4 border border-gray-200 rounded-lg dark:border-gray-700 mt-14">
+            <div className="p-4 border border-gray-200 rounded-sm dark:border-gray-700 mt-14">
                 <Outlet/>
             </div>
         </>
